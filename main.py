dd13d7fdc9bd1152d5a1142bc0b979d803d864e0
roknuzzaman <rokon.raz@gmail.com>
2025-11-13 13:02:02 +0600

    Update local port.

--- main.py
+++ main.py
@@ -65,7 +65,12 @@ app.add_middleware(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:3001",
+        "*"  # Allow all origins (remove in production for better security)
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
